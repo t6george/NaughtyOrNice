ecdd443c7f9076f76a472c7fd90fedf14bddc611
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-02-24 17:54:09 -0500

    transferring the neural net into a class and coding backpropagation algorithm from scratch ethereum

--- employabilityImageClassification.py
+++ employabilityImageClassification.py
@@ -1,5 +1,48 @@
 import tensorflow as tf
 import numpy as np
+import random
+
+
+def sigmoid(z):
+    return 1.0/(1.0 + tf.exp(-z))
+def sigmoid_prime(z):
+    return sigmoid(z)*(1-sigmoid(z))
+
+class Network(object):
+
+    def __init__(self,layers):
+        self.num_layers = len(layers)
+        self.layers = layers
+        self.weights = [tf.random_normal(shape = [y,x],stddev=0.4) for x,y in zip(layers[1:],layers[:-1])]
+        self.biases = [tf.random_normal(shape = [y,1], stddev=0.4) for y in layers[:-1]]
+    def feedforward(self,inp):
+        for bias,weight in zip(self.biases,self.weights):
+            inp = sigmoid(tf.add(tf.matmul(inp,weight),bias))
+        return inp
+    def stochastic_gradient_descent(self,training_data,batch_size,epochs,rate,test_data=None):
+        n = len(training_data)
+        for epoch in range(epochs):
+            random.shuffle(training_data)
+            batches = [training_data[i:i+batch_size] for i in range(0,n,batch_size)]
+            for batch in batches:
+                b_differential = [tf.zeros(shape=bias.get_shape(),dtype='float32',name='nabla_b') for bias in self.biases]
+                w_differential = [tf.zeros(shape=weight.get_shape(),dtype='float32',name='nabla_w') for weight in self.weights]
+                for inp,out in batch:
+                    d_b_differential, d_w_differential = self.backpropagate(inp,out)
+                    w_differential = [i+j for i,j in zip(b_differential, d_b_differential)]                
+                    w_differential = [i+j for i,j in zip(w_differential, d_w_differential)]                
+            if test_data:
+                test_count = len(test_data)
+            if test_data:
+                print('Epoch #:',epoch,'accurate to:',self.evaluate(test_data),'/',test_count)
+            else:
+                print('Epoch #:',epoch,'done')
+            
+        
+        
+            
+    
+
 
 inputs=tf.placeholder('float',[None,2],name='Input')
 targets=tf.placeholder('float',name='Target')
